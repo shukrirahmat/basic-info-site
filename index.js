295c20a6968810ee709cd5a77acb4de27c32a24b
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-11-10 23:11:35 +0800

    reformatted code

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-    let pagePath;
-    let statsCode = 200;
-    if (req.url === "/") {
-        pagePath = path.join(__dirname, 'public', 'index.html');
-    } else if (req.url === "/about") {
-        pagePath = path.join(__dirname, 'public', 'about.html');
-    } else if (req.url === "/contact-me") {
-        pagePath = path.join(__dirname, 'public', 'contact-me.html');
-    } else {
-        pagePath = path.join(__dirname, 'public', '404.html');
-        statsCode = 404;
-    }
+  let fileName = req.url === "/" ? "/index.html" : `${req.url}.html`;
+  let filePath = path.join(__dirname, "public", fileName);
 
-    fs.readFile(pagePath, (err, data) => {
-        if (err) throw err;
-        res.writeHead(statsCode, {'Content-Type' : 'text/html'});
-        res.end(data)
-    })
-})
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      let errorPath = path.join(__dirname, "public", "404.html");
+      fs.readFile(errorPath, (error, errorPage) => {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end(errorPage);
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+});
 
 server.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}...`);
-})
-
+  console.log(`Listening to port ${PORT}...`);
+});
